0ffd775d685dd9c957fb82f1e1b2af1ae71cfd81
korticux <luis_cortina18@hotmail.com>
2022-08-08 21:09:55 -0700

    Models, Tables and Controllers Admin

---
+++ database/migrations/2022_08_09_035815_create_estatus_propiedads_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstatusPropiedadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estatus_propiedads', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estatus_propiedads');
+    }
+}
