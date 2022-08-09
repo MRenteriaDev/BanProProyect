1686665fe5ff5fd74e9ceeb123d00738263277f4
korticux <luis_cortina18@hotmail.com>
2022-08-08 22:24:28 -0700

    add tables pt2

---
+++ database/migrations/2022_08_09_051916_add_correo_to_solicitud_vendedors.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCorreoToSolicitudVendedors extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('solicitud_vendedors', function (Blueprint $table) {
+            $table->string("correo")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('solicitud_vendedors', function (Blueprint $table) {
+            $table->string("correo")->nullable();
+        });
+    }
+}
