3bc2b3e729d5672b2866f23f2b8debbcee2d7656
MRenteriaDev <mrenteriadev@outlook.com>
2022-08-08 19:49:08 -0700

    Seeder and template ready

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'usuario BanPro',
+            'email' => 'usuario@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        DB::table('admins')->insert([
+            'name' => 'Admin BanPro',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        DB::table('sellers')->insert([
+            'name' => 'Seller BanPro',
+            'email' => 'seller@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
