ac42d9c6200888834af4881f9c0b3a6cb0352b1c
korticux <luis_cortina18@hotmail.com>
2022-08-11 00:36:55 -0700

    Welcome page fix

---
+++ resources/views/body/partners.blade.php
@@ -0,0 +1,20 @@
+<div class="partners bg-white rec-pro">
+    <div class="container-fluid">
+        <div class="sec-title">
+            <h2><span>Our </span>Partners</h2>
+            <p>The Companies That Represent Us.</p>
+        </div>
+        <div class="owl-carousel style2">
+            <div class="owl-item" data-aos="fade-up"><img src="images/partners/11.jpg" alt=""></div>
+            <div class="owl-item" data-aos="fade-up"><img src="images/partners/12.jpg" alt=""></div>
+            <div class="owl-item" data-aos="fade-up"><img src="images/partners/13.jpg" alt=""></div>
+            <div class="owl-item" data-aos="fade-up"><img src="images/partners/14.jpg" alt=""></div>
+            <div class="owl-item" data-aos="fade-up"><img src="images/partners/15.jpg" alt=""></div>
+            <div class="owl-item" data-aos="fade-up"><img src="images/partners/16.jpg" alt=""></div>
+            <div class="owl-item" data-aos="fade-up"><img src="images/partners/17.jpg" alt=""></div>
+            <div class="owl-item" data-aos="fade-up"><img src="images/partners/11.jpg" alt=""></div>
+            <div class="owl-item" data-aos="fade-up"><img src="images/partners/12.jpg" alt=""></div>
+            <div class="owl-item" data-aos="fade-up"><img src="images/partners/13.jpg" alt=""></div>
+        </div>
+    </div>
+</div>
