3674406f67035fa8c778447a0a88745039e734a0
korticux <luis_cortina18@hotmail.com>
2022-08-16 21:35:37 -0700

    update zonas

---
+++ resources/views/admin/review_sellers/update.blade.php
@@ -0,0 +1,65 @@
+@extends('admin.admin_master')
+
+@section('admin')
+    <section class="content-header" style="margin-top: 10px">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Review Sellers</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-md-right">
+                        <a class="btn btn-success btn-md" href="{{ route('reviewsellers.index') }}"><i class="fas fa-arrow-left"></i>
+                            &nbsp; Volver a Inicio
+                        </a>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="col-md-12">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Creación de Review Seller</h3>
+            </div>
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form method="POST" action="{{ route('reviewsellers.update', $review_seller->id) }}">
+                @csrf
+                <div class="card-body col-md-12">
+                    <div class="form-group">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" value="{{ $review_seller->nombre }}" name="nombre" class="form-control" id="nombre" placeholder="Enter name">
+                        @error('nombre')
+                            <span class="text-danger"> {{ $message }} </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="descripcion">Descripcion</label>
+                        <textarea class="form-control" value="{{ $review_seller->descripcion }}" name="descripcion" id="descripcion" cols="10" rows="2"></textarea>
+                        @error('descripcion')
+                            <span class="text-danger"> {{ $message }} </span>
+                        @enderror
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>Seleccionar vendedor</label>
+                            <select class="form-control select2" name="seller_id" style="width: 100%;height: 100%;">
+                                <option selected="selected">Seleccionar</option>
+                                @foreach ($sellers as $seller)
+                                    <option value={{ $seller->id }}>{{ $seller->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Crear</button>
+                    <a href="{{ route('reviewsellers.index') }}" class="btn btn-danger">Cancelar</a>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
