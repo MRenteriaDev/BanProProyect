bbbf3198409f688a6bf4f4a960cc33a97814a5ec
korticux <luis_cortina18@hotmail.com>
2022-08-22 22:39:40 -0700

    bootstrap edit

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,62 @@
 @extends('admin.admin_master')
-
-
 @section('admin')
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Test</h1>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
 
-   
+<hr>
+<div class="row" style="padding-left: 250px;">
+
+    <div class="col-lg-3 col-6">
+
+    <div class="small-box bg-info">
+    <div class="inner">
+    <h3>@php
+        use App\Models\Propiedades;
+        $ca = Propiedades::count();
+        echo($ca);
+    @endphp</h3>
+    <p>Propiedades registradas</p>
+    </div>
+    <div class="icon">
+        <i class="fa-solid fa-building"></i>
+    </div>
+    </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+
+    <div class="small-box bg-success">
+    <div class="inner">
+    <h3>@php
+        use App\Models\Seller;
+        $cr = Seller::count();
+        echo($cr);
+    @endphp</h3>
+    <p>Sellers registrados</p>
+    </div>
+    <div class="icon">
+        <i class="fa-solid fa-user-group"></i>
+    </div>
+    </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+
+    <div class="small-box bg-warning">
+    <div class="inner">
+    <h3>@php
+        use App\Models\Reviews;
+        $ce = Reviews::count();
+        echo($ce);
+    @endphp</h3>
+    <p>Reviews registradas</p>
+    </div>
+    <div class="icon">
+    <i class="fa fa-file-text">
+    </i>
+    </div>
+    </div>
+    </div>
+
+    </div>
 @endsection
+
+
