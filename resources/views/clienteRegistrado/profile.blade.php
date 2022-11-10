6b90d39348762bc0326bdeb1a1a7260fa0a2fdb2
Lekaba <LCT_jorge@hotmail.com>
2022-11-09 23:10:31 -0700

    dashboard v1

--- resources/views/clienteRegistrado/profile.blade.php
+++ resources/views/clienteRegistrado/profile.blade.php
@@ -2,21 +2,32 @@
 
 
 @section('cliente-registrado-dashboard')
+    @php
+        if(Auth::user()->stauts == 1){
+            $estado_membresia = "Pagado";
+        }
+        elseif(Auth::user()->stauts == 0){
+            $estado_membresia = "Pago pendiente";
+        }
+
+
+    @endphp
     <div class="widget-boxed-header">
         <h4>Detalles Del Perfil</h4>
     </div>
     <div class="sidebar-widget author-widget2">
         <div class="author-box clearfix">
 
-            <img src="{{ Auth::user()->avatar ?? '[$fd]' }}" >
+            <img src="{{ Auth::user()->avatar ?? '[$fd]' }}">
             <h4 class="author__title">{{ Auth::user()->name }}</h4>
             <p class="author__meta">Usuario</p>
+            <p class="author__meta">Estado de membresia: {{$estado_membresia}}</p>
         </div>
         <ul class="author__contact">
             <li><span class="la la-envelope-o"><i class="fa fa-envelope" aria-hidden="true"></i></span><a
                     href="#">{{ Auth::user()->email }}</a></li>
         </ul>
-        <div class="agent-contact-form-sidebar">
+        {{-- <div class="agent-contact-form-sidebar">
             <h4>Realizar Cambio </h4>
             <form autocomplete="off" method="POST" action="{{ route('user.update', Auth::user()->id) }}">
                 @csrf
@@ -27,6 +38,6 @@
                 <button type="submit" class="btn btn-primary">Realizar Cambio </button>
 
             </form>
-        </div>
+        </div> --}}
     </div>
 @endsection
