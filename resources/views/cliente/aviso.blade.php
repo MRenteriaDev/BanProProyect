963a60f28942de792ab252c66d7156c93ec2534e
korticux <luis_cortina18@hotmail.com>
2022-11-25 16:44:46 -0700

    aviso de privacidad contenido

--- resources/views/cliente/aviso.blade.php
+++ resources/views/cliente/aviso.blade.php
@@ -2,27 +2,15 @@
 @section('cliente')
     <section id="hero-area" class="parallax-searchs home15 overlay thome-6 thome-1" data-stellar-background-ratio="1">
 
-
-
-        <div class="card-body">
-            <div class="row">
-                <div class="col-sm-4">
-                    <div class="position-relative p-3 bg-gray" style="height: 180px">
-                        <div class="ribbon-wrapper">
-                            <div class="ribbon bg-primary">
-                                Ribbon
-                            </div>
-                        </div>
-                        Ribbon Default <br />
-                        <small>.ribbon-wrapper.ribbon-lg .ribbon</small>
-                    </div>
-                </div>
+    </section>
+    <section class="featured portfolio rec-pro disc">
+        <div class="container-fluid">
+            <div class="sec-title discover">
+                <h2><span>Aviso  </span>De Privacidad</h2>
+                <p>Contenido</p>
             </div>
+
         </div>
-
-
-
-
     </section>
     @include('cliente.body.footer')
 @endsection
